01bd5aa85a3c7de585e74f198b59d723562ac668
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-06-07 11:50:57 +0530

    done

--- CustomerLoanApplication/src/main/java/com/cjc/demo/controller/CustomerController.java
+++ CustomerLoanApplication/src/main/java/com/cjc/demo/controller/CustomerController.java
@@ -1,16 +1,8 @@
 package com.cjc.demo.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cjc.demo.dto.CustomerDto;
-import com.cjc.demo.model.Customer_Details;
 import com.cjc.demo.service.CustomerService;
 
 @RestController
@@ -19,11 +11,5 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerservice;
 
-	@PostMapping("/postCustomer")
-	public ResponseEntity<CustomerDto> postCustomer(@RequestBody Customer_Details customer)
-	{
-		customerservice.saveCustomerDetails(customer);
-		CustomerDto response=new CustomerDto("Customer details saved",new Date());
-		return new ResponseEntity<CustomerDto>(response,HttpStatus.CREATED);
-	}
+	
 }
