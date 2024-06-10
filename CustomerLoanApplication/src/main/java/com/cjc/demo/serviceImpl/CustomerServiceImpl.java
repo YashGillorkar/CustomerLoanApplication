86747fbcc6c6600bd2fe56f256aa9adfd8ed8cae
drvik <drvik@LENOVO>
2024-06-10 15:03:55 +0530

    Updated CustomerSanctionData using put method

--- CustomerLoanApplication/src/main/java/com/cjc/demo/serviceImpl/CustomerServiceImpl.java
+++ CustomerLoanApplication/src/main/java/com/cjc/demo/serviceImpl/CustomerServiceImpl.java
@@ -1,15 +1,19 @@
 package com.cjc.demo.serviceImpl;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cjc.demo.exception.InvaliedCustomerId;
 import com.cjc.demo.model.AllPersonalDocument;
+import com.cjc.demo.model.CustomerVerification;
 import com.cjc.demo.model.Customer_Cibil_Score;
 import com.cjc.demo.model.Customer_Details;
+import com.cjc.demo.model.Customer_Saction_Letter;
 import com.cjc.demo.repository.CustomerRepository;
 import com.cjc.demo.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -57,6 +61,20 @@ public class CustomerServiceImpl implements CustomerService {
 
 		} catch (IOException ie) {
 			ie.printStackTrace();
+		}
+	}
+
+	@Override
+	public void saveSactionDetails(int customerId, Customer_Saction_Letter customerSactionLetter) {
+	
+		Optional<Customer_Details> op=customerrepository.findById(customerId);
+		if(op.isPresent()) {
+			Customer_Details customer_Details = op.get();
+			customer_Details.setCustomersactionletter(customerSactionLetter);
+			customerrepository.save(customer_Details);
+		}
+		else {
+			throw new InvaliedCustomerId("invaild customer id");
 		}
 	}
 }
